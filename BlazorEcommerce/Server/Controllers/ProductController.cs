5e82c9452bc9efd53ca63bcefecc0632f90a9f88
Prince Regencia <princejearegencia@gmail.com>
2023-09-27 20:40:01 +0800

    #Lesson no.3  Add and use best practices

--- BlazorEcommerce/Server/Controllers/ProductController.cs
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -7,16 +7,17 @@ namespace BlazorEcommerce.Server.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        private DataContext _context;
-        public ProductController(DataContext context)
+        private IProductService _productService;
+
+        public ProductController(IProductService productService)
         {
-            _context = context;
+            _productService = productService;
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(){
-            var products = await _context.Products.ToListAsync();
-            return Ok(products);
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProducts(){
+           var result = await _productService.GetProductListAsync();
+            return Ok(result);
         }
     }
 }
---
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace BlazorEcommerce.Server.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+        private DataContext _context;
+
+        public ProductService(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<ServiceResponse<List<Product>>> GetProductListAsync()
+        {
+            var response = new ServiceResponse<List<Product>>()
+            {
+                Data = await _context.Products.ToListAsync()
+            };
+            return response;
+        }
+    }
+}
