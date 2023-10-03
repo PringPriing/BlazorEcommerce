23bdedf2be34ef4afe80d1dd44257897ff0412e3
Prince Regencia <princejearegencia@gmail.com>
2023-10-03 15:25:52 +0800

    Added category service and refactor index page

--- BlazorEcommerce/Client/Program.cs
+++ BlazorEcommerce/Client/Program.cs
@@ -3,6 +3,7 @@ global using BlazorEcommerce.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorEcommerce.Client.Services.ProductService;
+using BlazorEcommerce.Client.Services.CategoryService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +11,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService,ProductService>();
-
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 await builder.Build().RunAsync();
--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -27,6 +27,18 @@ namespace BlazorEcommerce.Server.Services.ProductService
             return response;
         }
 
+        public async Task<ServiceResponse<List<Product>>> GetProductByCategoryAsync(string categoryUrl)
+        {
+            var response = new ServiceResponse<List<Product>>()
+            {
+                Data = await _context.Products
+                        .Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
+                        .ToListAsync()
+                
+            };
+            return response;
+        }
+
         public async Task<ServiceResponse<List<Product>>> GetProductListAsync()
         {
             var response = new ServiceResponse<List<Product>>()
