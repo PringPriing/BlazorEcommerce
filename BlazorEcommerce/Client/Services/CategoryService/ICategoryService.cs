1843086817d4eee6918bfbc333c00c01b688919d
Prince Regencia <princejearegencia@gmail.com>
2024-04-30 12:59:25 +0800

    add administration for category

--- BlazorEcommerce/Client/Services/CategoryService/ICategoryService.cs
+++ BlazorEcommerce/Client/Services/CategoryService/ICategoryService.cs
@@ -2,6 +2,13 @@
 
 public interface ICategoryService
 {
+    event Action OnChange;
+    List<Category> AdminCategories { get; set; }
     List<Category> Categories { get; set; }
     Task GetCategories();
+    Task GetAdminCategories();
+    Task AddCategory(Category category);
+    Task UpdateCategory(Category category);
+    Task DeleteCategory(int categoryId);
+    Category CreateNewCategory();
 }
